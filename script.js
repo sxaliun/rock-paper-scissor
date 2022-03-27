0e193c99a6a8aac85f5105d2be6c9b81b5724771
Khaliun <83344867+sxaliun@users.noreply.github.com>
2022-03-27 15:06:32 +0800

    got a way to count scores of each player. Now, this code needs to be looped until someone's scored 5

--- script.js
+++ script.js
@@ -8,28 +8,37 @@ let computerSelection = function () {
     return computerChoice[Math.floor(Math.random()*computerChoice.length)];
 };
 
+let playerScore = 0;
+let compScore = 0;
+
 function playRound (playerSelection, computerSelection) {
     if (computerSelection == 'paper' && playerSelection == 'scissor') {
-            return 'Computer chose paper. Scissor beats the paper!';
-        } else if (computerSelection == 'rock' && playerSelection == 'paper') {
-            return 'Computer chose rock. Paper beats the rock!';
-        }else if (computerSelection == 'scissor' && playerSelection == 'rock') {
+        playerScore++;
+        return 'Computer chose paper. Scissor beats the paper!';
+    } else if (computerSelection == 'rock' && playerSelection == 'paper') {
+        playerScore++;
+        return 'Computer chose rock. Paper beats the rock!';
+    }else if (computerSelection == 'scissor' && playerSelection == 'rock') {
+            playerScore++;
             return 'Computer chose scissor. Rock beats the scissor!';
-        }else if (computerSelection == 'scissor' && playerSelection == 'paper') {
+    }else if (computerSelection == 'scissor' && playerSelection == 'paper') {
+            compScore++;
             return 'Computer chose scissor. Paper is beaten by the scissor!';
-        } else if (computerSelection == 'paper' && playerSelection == 'rock') {
+    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
+            compScore++;
             return 'Computer chose paper. Rock is beaten by the paper!';
-        }else if (computerSelection == 'rock' && playerSelection == 'scissor') {
+    }else if (computerSelection == 'rock' && playerSelection == 'scissor') {
+            compScore++;
             return 'Computer chose rock. Scissor is beaten by the rock!';
-        } else if (computerSelection == 'scissor' && playerSelection == 'scissor') {
+    } else if (computerSelection == 'scissor' && playerSelection == 'scissor') {
             return "Computer chose scissor. It's a tie!";
-        } else if (computerSelection == 'rock' && playerSelection == 'rock') {
+    } else if (computerSelection == 'rock' && playerSelection == 'rock') {
             return "Computer chose rock. It's a tie!";
-        } else if (computerSelection == 'paper' && playerSelection == 'paper') {
+    } else if (computerSelection == 'paper' && playerSelection == 'paper') {
             return "Computer chose paper. It's a tie!";
-        } else {
+    } else {
             return "it's not working!"
-        }
+    }
 }
 
 function game () {
