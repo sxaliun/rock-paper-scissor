63afff996e2e126a04595a9b4370c47c8639325e
Khaliun <83344867+sxaliun@users.noreply.github.com>
2022-04-12 23:22:34 +0800

    cool styles and html elements are added. also, js is restructud as dom elements are manipulated --- so many changes in one go --- but been doing this for like a week

--- script.js
+++ script.js
@@ -1,65 +1,92 @@
-let playerSelection = function () {
-    let playerChoice = prompt ('Choose your weapon: rock, paper or scissor?');
-    return playerChoice.toLowerCase ();
-};
+const selectionBtns = document.querySelectorAll('button');
+selectionBtns.forEach(button => button.addEventListener('click', getPlayerChoice));
+
+let playerChoice;
+function getPlayerChoice (e) {
+  let playerSelection = e.target.id;
+  playerChoice = e.target.textContent.toLowerCase();
+  console.log('Player: ' +playerSelection);
+  playRound(playerSelection, computerSelection());
+  // console.log('Player: ${playerScore}, Computer: ${compScore}');
+}
 
 let computerSelection = function () {
-    let computerChoice = ['rock', 'paper', 'scissor'];
-    return computerChoice[Math.floor(Math.random()*computerChoice.length)];
+  let computerOptions = ['rock', 'paper', 'scissor'];
+  let computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+  console.log('Computer: '+ computerChoice);
+  return computerChoice;
 };
 
 let playerScore = 0;
 let compScore = 0;
 
 function playRound (playerSelection, computerSelection) {
-    if (computerSelection == 'paper' && playerSelection == 'scissor') {
-        playerScore++;
-        return 'Computer chose paper. Scissor beats the paper!';
-    } else if (computerSelection == 'rock' && playerSelection == 'paper') {
-        playerScore++;
-        return 'Computer chose rock. Paper beats the rock!';
-    }else if (computerSelection == 'scissor' && playerSelection == 'rock') {
-            playerScore++;
-            return 'Computer chose scissor. Rock beats the scissor!';
-    }else if (computerSelection == 'scissor' && playerSelection == 'paper') {
-            compScore++;
-            return 'Computer chose scissor. Paper is beaten by the scissor!';
-    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
-            compScore++;
-            return 'Computer chose paper. Rock is beaten by the paper!';
-    }else if (computerSelection == 'rock' && playerSelection == 'scissor') {
-            compScore++;
-            return 'Computer chose rock. Scissor is beaten by the rock!';
-    } else if (computerSelection == 'scissor' && playerSelection == 'scissor') {
-            return "Computer chose scissor. It's a tie!";
-    } else if (computerSelection == 'rock' && playerSelection == 'rock') {
-            return "Computer chose rock. It's a tie!";
-    } else if (computerSelection == 'paper' && playerSelection == 'paper') {
-            return "Computer chose paper. It's a tie!";
-    } else {
-            return "Please choose a valid weapon!"
-    }
-}
+  
+  //TIE CONDITION
+  if (computerSelection === playerSelection) {
+    console.log(`It's a tie!`);
+  }
 
-//repeat the game (for) until (break) one of the player gets 5 points first (if)
+  //PLAYER WINNING CONDITIONS
 
-function playGame () {
-    for (let i = 0; ; i++){
-        console.log(playRound(playerSelection(), computerSelection()));
-        if (compScore===5 || playerScore===5) {
-            console.log (`Computer scored ${compScore} and Player scored ${playerScore}!`);
-            console.log(checkWinner ());
-    break
-    }
+  else if (
+    (playerSelection === 'scissor' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'rock' && computerSelection === 'scissor') ) {
+    playerScore++;
+    console.log(`Player beats!`);
+    console.log('Player: '+playerScore+' VS '+'Computer:'+compScore);
+    
+  }
+  //   //COMPUTER WINNING CONDITIONS
+  
+  else if (
+    (playerSelection === 'paper' && computerSelection === 'scissor')||
+    (playerSelection === 'rock' && computerSelection === 'paper')||
+    (playerSelection === 'scissor' && computerSelection === 'rock')
+    )
+    {
+      compScore++;
+      console.log(`Computer beats!`);
+      console.log('Player: '+playerScore+' VS '+'Computer:'+compScore);
+      return 'Computer chose scissor. Paper is beaten by the scissor!';
+    } 
+  else {
+    console.log('Your code is not working!')
+  }
 }
-}
-
-//decide who wins and declare the winner
 
 function checkWinner (){
-    if (compScore>playerScore) {
-        console.log('Computer wins the game!🏆');
-    }else {
-        console.log('Player wins the game!🏆');
-    }
+  if (compScore>playerScore) {
+    console.log('Computer wins the game!🏆');
+  }else {
+    console.log('Player wins the game!🏆');
+  }
 }
+
+//(for) repeat the game until (break) one of the player gets 5 points first (if)
+
+// function playGame () {
+//     for (let i = 0; ; i++){
+//       console.log(playRound(playerSelection(), computerSelection()));
+//       if (compScore === 5 || playerScore === 5) {
+//             console.log (`Computer scored ${compScore} and Player scored ${playerScore}!`);
+//           console.log(checkWinner ());
+//   break
+//   }
+// }
+// }
+
+//decide who wins and announce the winner
+
+// let playerSelection = function () {
+//   if (){
+//     const playerRock = document.querySelector('.rock');
+//     playerRock.addEventListener('click', ()=>{
+
+//     })
+//   }
+//   let playerChoice = ();
+//   console.log(playerChoice);
+//   return playerChoice.toLowerCase ();
+// };
